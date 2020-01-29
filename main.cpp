c47e32b720fdcc8a6a917ca268db9002bf7742b9
John Higdon <jmhigdon@gmail.com>
2020-01-29 11:32:21 -0800

    Added user input and printed results.

--- main.cpp
+++ main.cpp
@@ -2,10 +2,15 @@
 
 using std::cin;
 using std::cout;
+using std::endl;
 
 int main()
 {
-    cout<<"Hi, please enter two numbers: ";
-
+    cout << "Hi, please enter two numbers: ";
+    int num1;
+    int num2;
+    cin >> num1;
+    cin >> num2;
+    cout << "The numbers entered were " << num1 << " and " << num2 << endl;
     return 0;
 }
