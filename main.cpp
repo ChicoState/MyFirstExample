2a4b902797b56d2f364b3608b5907fcc4348afe0
John Higdon <jmhigdon@gmail.com>
2020-01-29 11:37:11 -0800

    Cleaned up code and output.

--- main.cpp
+++ main.cpp
@@ -6,11 +6,10 @@ using std::endl;
 
 int main()
 {
-    cout << "Hi, please enter two numbers: ";
-    int num1;
-    int num2;
+    cout << "Hi, please enter two integers. Press enter after entering said integer: ";
+    int num1, num2;
     cin >> num1;
     cin >> num2;
-    cout << "The numbers entered were " << num1 << " and " << num2 << endl;
+    cout << "The integers entered were " << num1 << " and " << num2 << endl;
     return 0;
 }
