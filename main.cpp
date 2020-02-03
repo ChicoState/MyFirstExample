2cbc996f4f37409aec998aa681ff25777d1699c9
John Higdon <jmhigdon@gmail.com>
2020-02-03 11:59:02 -0800

    Divided first number by the second and printed results.

--- main.cpp
+++ main.cpp
@@ -4,24 +4,12 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-int main()
-{
-
-    cout << "Hi, please enter two integers. Press enter after entering said integer: ";
+int main() {
+    cout << "Hi, please enter two integers: ";
     int num1, num2;
-    cin >> num1;
-    cin >> num2;
-    cout << "The integers entered were " << num1 << " and " << num2 << endl;
-    cout << "Below is the amazing merger." << endl;
-
-
-
-    cout<<"Hi, please enter two numbers: ";
-
-    int x,y;
-
-    cin >> x >> y;
-    cout << x + y << endl;
-
+    cin >> num1 >> num2;
+    int result = num1 / num2;
+    cout << "The integer division result of " << num1 << " divided by " 
+	 << num2 << " is " << result << endl;
     return 0;
 }
