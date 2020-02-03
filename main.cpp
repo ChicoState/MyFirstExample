064c373565dec0da85645a5570fe5f6b8fe83ade
John Higdon <jmhigdon@gmail.com>
2020-02-03 12:10:18 -0800

    Handles division by zero errors.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,10 @@ int main() {
     cout << "Hi, please enter two integers: ";
     int num1, num2;
     cin >> num1 >> num2;
+    while (num2 == 0) {
+      cout << "We cannot divide by zero, please enter a non-zero integer: ";
+      cin >> num2; 
+    }
     int result = num1 / num2;
     cout << "The integer division result of " << num1 << " divided by " 
 	 << num2 << " is " << result << endl;
