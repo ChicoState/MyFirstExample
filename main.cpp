fecc57ba2e3d5ac7bff627a5c6967561f876f94c
John Higdon <jmhigdon@gmail.com>
2020-02-03 12:21:46 -0800

    Cleaned up spacing and added comments.

--- main.cpp
+++ main.cpp
@@ -6,14 +6,20 @@ using std::endl;
 
 int main() {
     cout << "Hi, please enter two integers: ";
+
     int num1, num2;
     cin >> num1 >> num2;
+
+    // handle division by zero
     while (num2 == 0) {
       cout << "We cannot divide by zero, please enter a non-zero integer: ";
       cin >> num2; 
     }
+
+    // print result
     int result = num1 / num2;
     cout << "The integer division result of " << num1 << " divided by " 
-	 << num2 << " is " << result << endl;
+         << num2 << " is " << result << "." << endl;
+
     return 0;
 }
