f5d7247b1795ed7ef5a89942d58ed0a0afa14e9e
John Higdon <jmhigdon@gmail.com>
2020-02-03 13:35:07 -0800

    Added functionality for exponents. Fixes #14.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <cmath>
 
 using std::cin;
 using std::cout;
 using std::endl;
 
 int main() {
-    cout << "Hi, please enter two integers which we will add and divide: ";
+    cout << "Hi, please enter two integers which we will add, divide and find the exponent of: ";
 
     int num1, num2;
     cin >> num1 >> num2;
@@ -19,8 +20,9 @@ int main() {
     // print results
     int result_add = num1 + num2;
     int result_div = num1 / num2;
+    int result_exp = pow(num1, num2);
     cout << num1 << " + " << num2 << " = " << result_add << endl;
     cout << num1 << " / " << num2 << " = " << result_div << endl;
-
+    cout << num1 << "^" << num2 << " = " << result_exp << endl;
     return 0;
 }
