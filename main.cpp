8ffc1da25759e3057eeca1a04ac330e3e84abf6e
Ramsfield <mgriffith004@mail.csuchico.edu>
2020-02-03 13:10:57 -0800

    Added divisibility functionality. Fixes #9 fixes #12 fixes #13

--- main.cpp
+++ main.cpp
@@ -4,14 +4,46 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+int divide(int, int);
+int add(int, int);
+
 int main()
 {
-    cout<<"Hi, please enter two numbers: ";
+    cout << "Hi, would you like to add or divide? ";
+    std::string input;
+    cin >> input;
+    cout<<"Please enter two numbers: ";
 
-    int x,y;
+    int first, second;
+    cin >> first >> second;
 
-    cin >> x >> y;
-    cout << x + y << endl;
+    if(input == "divide")
+    {
+      if(second == 0)
+      {
+        cout << "You can not divide by 0\n";
+        exit(1);
+      }
+      cout << divide(first, second) << endl;
+    }
+    else if(input == "add")
+    {
+      cout << add(first, second) << endl;
+    }
+    else
+    {
+      cout << "Option not supported. Please try again.\n";
+    }
 
     return 0;
+}
+
+int add(int x, int y)
+{
+  return x+y;
+}
+
+int divide(int x, int y)
+{
+  return (y == 0) ? 0 : x/y;
 }
