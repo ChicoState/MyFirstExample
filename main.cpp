fe3738a8dd0b43ede8033239cf01e7193a52efbd
th3n1ghmanc0m3th <jaywehrman@gmail.com>
2020-09-02 11:35:43 -0700

    Adds mult to output, resolves issue#19

--- main.cpp
+++ main.cpp
@@ -6,12 +6,20 @@ using std::endl;
 
 int main()
 {
+    cout<<"Laughs are cheap dude, I'm going for gasps!";
     cout<<"Hi, please enter two numbers: ";
 
     int x,y;
 
     cin >> x >> y;
     cout << x + y << endl;
+    cout << x * y << endl;
+    if(x == 0 || y == 0) {
+        cout << "WE don't play that here." << endl;
+    }
+    else {
+        cout << x / y << endl;
+    }
 
     return 0;
 }
