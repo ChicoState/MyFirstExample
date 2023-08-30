cd47a27d07ba1f0dae08990c6fc0dbf31c86da31
Matthew Martinez <mmartinez18@csuchico.edu>
2023-08-30 12:14:31 -0700

    added error message for divide by 0

--- main.cpp
+++ main.cpp
@@ -11,7 +11,11 @@ int main()
     std::cout << "Addition: " << x + y << std::endl;
     std::cout << "Subtraction: " << x - y << std::endl;
     std::cout << "Multiplication: " <<x * y << std::endl;
+    if(x!=0){
     std::cout << "Division: " << x / y << std::endl;
+    } else{
+        std::cout << "Cannot divide by zero" << std::endl;
+    }
     std::cout << "Remainder: " <<x % y << std::endl;
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
