a3552a54be05f7a887714a122714c336cf068698
JRobbinsss18 <jrobbinsss18@gmail.com>
2023-08-30 11:47:34 -0700

    resolved

--- main.cpp
+++ main.cpp
@@ -15,10 +15,19 @@ int main()
     cout << "Addition: " << x + y << endl;
     cout << "Subtraction: " << x - y << endl;
     cout << "Multiplication: " <<x * y << endl;
-    cout << "Division: " << x / y << endl;
+    if (y != 0){
+        cout << "Division: " << x / y << endl;
+    }
+    
+    else
+            cout << "Dividing by zero is not a number." << endl;
+    if (y != 0){
     cout << "Remainder: " <<x % y << endl;
+    }
+    else
+            cout << "Dividing by zero is not a number." << endl;
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
-
+    
     return 0;
 }
