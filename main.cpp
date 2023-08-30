543a9d494c018aa25814148b0d4c67bb2d18a666
collinsjacob127 <collinsjacob127@gmail.com>
2023-08-30 11:47:10 -0700

    Fix #62

--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@ int main()
     int x,y;
 
     std::cin >> x >> y;
-    std::cout << x << "+" << y << "=" << x + y << endl;
-    std::cout << x << "-" << y << "=" << x - y << endl;
-    std::cout << x << "*" << y << "=" << x * y << endl;
-    std::cout << x << "/" << y << "=" << x / y << " with remainder of " << x % y <<  endl;
-    std::cout << "Square Root of " << x << " is " << sqrt(x) << endl;
-    std::cout << "Square Root of " << y << " is " << sqrt(y) << endl;
-    std::cout << x << "^" << y << "=" << pow(x, y) << endl;
+    std::cout << x << "+" << y << "=" << x + y << std::endl;
+    std::cout << x << "-" << y << "=" << x - y << std::endl;
+    std::cout << x << "*" << y << "=" << x * y << std::endl;
+    std::cout << x << "/" << y << "=" << x / y << " with remainder of " << x % y <<  std::endl;
+    std::cout << "Square Root of " << x << " is " << sqrt(x) << std::endl;
+    std::cout << "Square Root of " << y << " is " << sqrt(y) << std::endl;
+    std::cout << x << "^" << y << "=" << pow(x, y) << std::endl;
 
     return 0;
 }
