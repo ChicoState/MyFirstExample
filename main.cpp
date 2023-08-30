6d0630c28b6280d0b7a157ba71bb2e9160458fc8
RioSakao <skoqush@gmail.com>
2023-08-30 11:34:59 -0700

    Resolved #61

--- main.cpp
+++ main.cpp
@@ -15,7 +15,10 @@ int main()
     cout << "Addition: " << x + y << endl;
     cout << "Subtraction: " << x - y << endl;
     cout << "Multiplication: " <<x * y << endl;
-    cout << "Division: " << x / y << endl;
+    if(y!=0)
+	cout << "Division: " << x / y << endl;
+    else
+	cout << "Dividing by zero is not a number." << endl;
     cout << "Remainder: " <<x % y << endl;
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
