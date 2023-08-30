5fdb1eb22781101ef81e9498153b73c41d4c29f9
tesss24 <tessshelton24@gmail.com>
2023-08-30 11:44:29 -0700

    changing output Resolved #62

--- main.cpp
+++ main.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <cmath>
+using std::cin;
+using std::cout;
+using std::endl;
 
 int main()
 {
     cout<<"Hi, please enter two whole numbers: ";
-
     int x,y;
-
-    std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " <<x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
-
+    cin >> x >> y;
+    cout << x << "+" << y << "=" << x+y << endl;
+    cout << x << "-" << y << "=" << x-y << endl;
+    cout << x << "*" << y << "=" << x*y << endl;
+    cout << x << "/" << y << "=" << x/y << " with a remainder of " << x % y << endl;
+    cout << "Square root of " << x << " is " << sqrt(x) << endl;
+    cout << "Square root of " << y << " is " << sqrt(y) << endl;
+    cout << x << "^" << y << "=" << pow(x, y) << endl;
     return 0;
 }
