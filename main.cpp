5419d9c710e0e3074a1479cab408516a27bf2285
Tony-Ton3 <95826645+Tony-Ton3@users.noreply.github.com>
2023-08-30 11:51:09 -0700

    first commit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 using std::cin;
 using std::cout;
@@ -11,11 +12,14 @@ int main()
     int x,y;
 
     cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
-    cout << "Multiplication: " <<x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " <<x % y << endl;
-
+    cout << x << "+" <<  y << "=" << x + y << endl;
+    cout << x << "-" <<  y << "=" << x - y << endl;
+    cout << x << "*" <<  y << "=" << x * y << endl;
+    int remainder = x % y;
+    cout << x << "/" <<  y << "=" << x / y  << "with remainder of" << remainder << endl;
+    cout << "Square root of " << x << " is " << sqrt(x) << endl;
+    cout << "Square root of " << y << " is " << sqrt(y) << endl;
+    cout << x << "^" <<  y << "=" << pow(x, y) << endl;
+    
     return 0;
 }
