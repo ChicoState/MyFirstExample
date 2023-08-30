10244a99363df8372fccc38e520bfecadeb3cfb3
Angel086 <aguerra688@gmail.com>
2023-08-30 11:47:35 -0700

    fixed issue #62

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <cmath>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -11,11 +11,15 @@ int main()
     int x,y;
 
     cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
-    cout << "Multiplication: " <<x * y << endl;
-    cout << "Division: " << x / y << endl;
+    cout << "Addition: " <<x<<"+"<<y<<"="<< x + y << endl;
+    cout << "Subtraction: " <<x<<"-"<<y<<"="<< x - y << endl;
+    cout << "Multiplication: " <<x<<"*"<<y<<"="<<x * y << endl;
+    cout << "Division: " <<x<<"/"<<y<<"="<< x / y << endl;
     cout << "Remainder: " <<x % y << endl;
-
+    cout<< "Square root of x: "<<sqrt(x) <<endl;
+    cout<< "Square root of y: "<<sqrt(y) <<endl;
+    cout<<x << " to the power of "<< y<< " is "<< pow(x,y)<<endl;
+/* add showing the maths and powers*/
     return 0;
+
 }
