dfdc50c7dae7f16dd5fc1e716c79967f5865d133
akachriss <chrisarobles27@gmail.com>
2023-08-30 12:30:49 -0700

    Added if statement checking for 2nd number 0.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,16 @@ int main()
     int x,y;
 
     std::cin >> x >> y;
+
+    if(y == 0){
+        std::cout << "Addition: " << x + y << std::endl;
+        std::cout << "Subtraction: " << x - y << std::endl;
+        std::cout << "Multiplication: " <<x * y << std::endl;
+        std::cout << "Dividing by zero is not a number." << std::endl;
+        std::cout << "Remainder: " <<x % y << std::endl;
+        std::cout << "Square Root: " << sqrt(x) << std::endl;
+        std::cout << "Square: " << pow(x, y) << std::endl;
+    }
     std::cout << "Addition: " << x + y << std::endl;
     std::cout << "Subtraction: " << x - y << std::endl;
     std::cout << "Multiplication: " <<x * y << std::endl;
