bda7e3873c95e154b6d74673527569a641170a0e
Shadowlief <shadowfire510@gmail.com>
2023-08-30 12:57:28 -0700

    added descriptions to opperations

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+include <ionstream>
 #include <cmath>
 
 int main()
@@ -8,13 +8,14 @@ int main()
     int x,y;
 
     std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " <<x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
+    std::cout << "Addition: " << x << "+" << y << "=" << x + y << std::endl;
+    std::cout << "Subtraction: " << x << "-" << y << "=" << x - y << std::endl;
+    std::cout << "Multiplication: " << x << "*" << y << "=" << x * y << std::endl;
+    std::cout << "Division: " << x << "/" << y << "=" << x / y << std::endl;
+    std::cout << x << "/" << y << "=" << x / y << " with remainder of " << x % y << std::endl;
+    std::cout << "Square Root of " << x << " is " << sqrt(x) << std::endl;
+    std::cout << "Square root of " << y << " is " << sqrt(y) << std::endl;
+    std::cout << "Square: " x << "^" << y << "=" << pow(x, y) << std::endl;
 
     return 0;
 }
