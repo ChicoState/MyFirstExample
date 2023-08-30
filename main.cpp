171378e6f54a95255cd9d1cdf2ccb7f73c71547f
Grant Meyer <gmeyer1@csuchico.edu>
2023-08-30 12:23:38 -0700

    resolved #61

--- main.cpp
+++ main.cpp
@@ -11,11 +11,13 @@ int main()
     std::cout << "Addition: " << x + y << std::endl;
     std::cout << "Subtraction: " << x - y << std::endl;
     std::cout << "Multiplication: " <<x * y << std::endl;
+    
     if(y!=0) {
       std::cout << "Division: " << x / y << std::endl;
       else {
         std:: cout << "Dividing by zero is not a number." << endl;
       }
+      
     std::cout << "Remainder: " <<x % y << std::endl;
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
