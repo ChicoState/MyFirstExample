b4f6ae5cfd4565a91844705d44add48092ede4b3
DarrianMC <darrianmcallister@gmail.com>
2023-08-30 11:40:25 -0700

    issue 61

--- main.cpp
+++ main.cpp
@@ -7,15 +7,21 @@ using std::endl;
 int main()
 {
     cout<<"Hi, please enter two whole numbers: ";
-
     int x,y;
 
     cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
-    cout << "Multiplication: " <<x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " <<x % y << endl;
+    if(y != 0)
+    {
 
+	    cout << "Addition: " << x + y << endl;
+   	 cout << "Subtraction: " << x - y << endl;
+    	cout << "Multiplication: " <<x * y << endl;
+   	 cout << "Division: " << x / y << endl;
+   	 cout << "Remainder: " <<x % y << endl;
+	}
+    else
+    {
+	    cout << "error";
+	   }
     return 0;
 }
