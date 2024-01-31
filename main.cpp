9629a1196861dab054fe94cbac2a62f849639dfd
Younitea <thomasrchiesa@gmail.com>
2024-01-31 12:09:25 -0800

    Fixes NaN when attempting to divide by 0 resolves #61

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    cout<<"Hi, please enter two whole numbers: ";
+    std::cout<<"Hi, please enter two whole numbers: ";
 
     int x,y;
 
@@ -11,8 +11,8 @@ int main()
     std::cout << "Addition: " << x + y << std::endl;
     std::cout << "Subtraction: " << x - y << std::endl;
     std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " <<x % y << std::endl;
+    std::cout << "Division: " << (y != 0 ? std::to_string(x / y) : ("Dividing by zero is not a number.")) << std::endl;
+    std::cout << "Remainder: " << (y != 0 ? std::to_string(x % y) : ("Dividing by zero is not a number.")) << std::endl;
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
 
