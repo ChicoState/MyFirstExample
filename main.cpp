2f2d45f9d565921eaead311812149f9d8c402ac4
KVentayen <cammeraprogramming@gmail.com>
2024-01-31 20:12:26 +0000

    Added expression for power function, resolves #62

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main()
     std::cout << "Division: " << x / y << std::endl;
     std::cout << "Remainder: " <<x % y << std::endl;
     std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
+    std::cout << "Square: " << x << '^' << y << '=' << pow(x, y) << std::endl;
 
     return 0;
 }
