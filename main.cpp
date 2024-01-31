3b336d420aaf0e222f50f2551bbc567eddd9f028
joshfollmer <josh.follmer@icloud.com>
2024-01-31 12:58:52 -0800

    fixed divide by 0 resolves #61

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ int main()
     
     std::cout << "Square Root of " << x << " is " << sqrt(x) << std::endl;
     std::cout << "Square Root of " << y << " is " << sqrt(y) << std::endl;
-    std::cout << x << "^" << y << "=" << pow(x, y) << std::endl;
-
+    std::cout << x << "^" << y << " = " << pow(x, y) << std::endl;
+    
     return 0;
 }
