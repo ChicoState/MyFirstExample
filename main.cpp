d9edac193e17a813c5c49ea4420b5148f1bee12d
droge91 <droge91@outlook.com>
2024-01-30 20:52:01 -0800

    Displays full math operations

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-#include <cmath>
+#include <math.h>
 
 int main()
 {
-    cout<<"Hi, please enter two whole numbers: ";
+    std::cout<<"Hi, please enter two whole numbers: ";
 
     int x,y;
 
     std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " <<x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
+    std::cout << "Addition: " << x << " + " << y << " = " << x + y << std::endl;
+        std::cout << "Subtraction: " << x << " - " << y << " = " << x - y << std::endl;
+        std::cout << "Multiplication: " << x << " * " << y << " = " << x * y << std::endl;
+        std::cout << "Division: " << x << "/" << y << " = " << x / y << std::endl;
+        std::cout << "Remainder: " << x << "/" << y << " = " << x / y << " with remainder of " << x % y << std::endl;
+        std::cout << "Square Root of " << x << " is " << sqrt(x) << std::endl;
+    std::cout << "Square Root of " << y << " is " << sqrt(y) << std::endl;
+        std::cout << "Square: " << x << "^" << y << "=" << pow(x, y);
 
     return 0;
 }
