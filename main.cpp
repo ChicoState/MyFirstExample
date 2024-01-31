492f0b377b1b3036ab4454f02145effe11052ffc
Anders Beil <akbeil@csuchico.edu>
2024-01-31 12:09:01 -0800

    Handle divinding by 0 resolves #61

--- main.cpp
+++ main.cpp
@@ -3,18 +3,23 @@
 
 int main()
 {
-    cout<<"Hi, please enter two whole numbers: ";
+  std::cout<<"Hi, please enter two whole numbers: ";
 
     int x,y;
 
     std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " <<x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
+    std::cout << x << "+" << y << "=" << x + y << std::endl;
+    std::cout << x << "-" << y << "=" << x - y << std::endl;
+    std::cout << x << "*" << y << "=" << x * y << std::endl;
+    if(y!=0){ 
+      std::cout << x << "/" << y << "=" << "with a remainder of" << x % y << std::endl;
+    }
+    else { 
+      std::cout << "Dividing by zero is not a number." << std::endl;
+    }
+    std::cout << "square root of " <<  x << " is " << sqrt(x) << std::endl;
+    std::cout << "square root of " << y << " is " << sqrt(y) << std::endl;
+    std::cout << x << "^" << y << "="  << pow(x, y) << std::endl;
 
     return 0;
 }
