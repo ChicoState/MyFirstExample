5780e67bf2aee2b79656b5066cd4d42453fc61b8
oliviajohnson91 <olivia@chicojohnsons.com>
2024-01-31 11:59:21 -0800

    Fixed spaces in output

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@ int main()
     std::cout << x << "-" << y << "=" << x - y << std::endl;
     std::cout << x << "*" << y << "=" << x * y << std::endl;
     std::cout << x << "/" << y << "=" << x / y << " with remainder of " << x%y << std::endl;
-    std::cout << "Square Root of " << x  << "is" << sqrt(x) << std::endl;
-    std::cout << "Square Root of " << y << "is" << sqrt(y) << std::endl;
+    std::cout << "Square Root of " << x  << " is " << sqrt(x) << std::endl;
+    std::cout << "Square Root of " << y << " is " << sqrt(y) << std::endl;
     std::cout << x << "^" << y << "=" << pow(x, y) << std::endl;
 
     return 0;
