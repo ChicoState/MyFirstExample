31bc1afbf1bc5d6203d49b56ab9d5ac8757a7600
Steven Thao <sthao21@csuchico.edu>
2024-01-31 12:12:34 -0800

    Addition

--- main.cpp
+++ main.cpp
@@ -3,20 +3,18 @@
 
 int main()
 {
-    cout<<"Hi, please enter two whole numbers: ";
+    std:: cout <<"Hi, please enter two whole numbers: ";
 
     int x,y;
 
     std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
+    std::cout << "Addition: " << x << "+" << y << "=" << x + y << std::endl;
     std::cout << "Subtraction: " << x - y << std::endl;
     std::cout << "Multiplication: " <<x * y << std::endl;
     std::cout << "Division: " << x / y << std::endl;
     std::cout << "Remainder: " <<x % y << std::endl;
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
-
-    std::cout << "Hello" << endl;
 
     return 0;
 }
