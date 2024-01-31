1d079c0e6886d2f72f9e3a787ffb6d3f4619b0e6
awroda596 <awroda596@gmail.com>
2024-01-31 12:23:44 -0800

    set namespace to std

--- main.cpp
+++ main.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <cmath>
-
+using namespace std; 
 int main()
 {
     cout<<"Hi, please enter two whole numbers: ";
 
     int x,y;
 
-    std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " <<x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
+    cin >> x >> y;
+    cout << "Addition: " << x + y << std::endl;
+    cout << "Subtraction: " << x - y << std::endl;
+    cout << "Multiplication: " <<x * y << std::endl;
+    cout << "Division: " << x / y << std::endl;
+    cout << "Remainder: " <<x % y << std::endl;
+    cout << "Square Root: " << sqrt(x) << std::endl;
+    cout << "Square: " << pow(x, y) << std::endl;
 
     return 0;
 }
