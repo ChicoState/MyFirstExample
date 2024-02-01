75e778c47ef7bb96efd559b304f0341e762bfdab
Kevin Buffardi <kbuffardi@csuchico.edu>
2024-01-31 21:21:25 -0800

    Refactor variable names for readability

--- main.cpp
+++ main.cpp
@@ -5,16 +5,16 @@ int main()
 {
     std::cout<<"Hi, please enter two whole numbers: ";
 
-    int x,y;
+    int first,second;
 
-    std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " <<x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
+    std::cin >> first >> second;
+    std::cout << "Addition: " << first + second << std::endl;
+    std::cout << "Subtraction: " << first - second << std::endl;
+    std::cout << "Multiplication: " << first * second << std::endl;
+    std::cout << "Division: " << first / second << std::endl;
+    std::cout << "Remainder: " << first % second << std::endl;
+    std::cout << "Square Root: " << sqrt(first) << std::endl;
+    std::cout << "Square: " << pow(first, second) << std::endl;
 
     return 0;
 }
