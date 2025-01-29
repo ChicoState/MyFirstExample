4c10a7ba626d8d3397568851da4a4c046f314c0c
Tyler <twenke@mail.csuchico.edu>
2025-01-29 11:46:48 -0800

    change cout text fix #61

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 
 int main()
 {
-    cout << "This is Kevin!\n";
-    cout<<"Hi, please enter two whole numbers: ";
+	std::cout << "This is Kevin!\n";
+	std::cout<<"Hi, please enter two whole numbers: ";
 
     int x,y;
 
     std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
+    std::cout << "x + y = " << x + y << std::endl;
+    std::cout << "x - y = " << x - y << std::endl;
     std::cout << "Multiplication: " <<x * y << std::endl;
     std::cout << "Division: " << x / y << std::endl;
     std::cout << "Remainder: " <<x % y << std::endl;
