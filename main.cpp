54a526c50f43a945ec79c3af2ae381291ecf78c7
cesar <cesargfranco270@gmail.com>
2025-01-29 11:47:56 -0800

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <cmath>
+using namespace std;
 
 int main()
 {
-    cout << "This is Kevin!\n";
+    cout << "This is Cesar!\n";
     cout<<"Hi, please enter two whole numbers: ";
 
     int x,y;
 
-    std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " <<x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
+    cin >> x >> y;
+    cout << "Addition: " << x << "+" << y << "=" << x + y << endl;
+    cout << "Subtraction: " << x << "-" << y << "=" << x - y << endl;
+    cout << "Multiplication: " << x << "*" << y << "=" << x * y << endl;
+    cout << "Division: " << x << "/" << y << "=" << x / y << " with Remainder of " << x % y << endl;
+    cout << "Square Root: " << "Square root of " << x << " is " << sqrt(x) << endl;
+    cout << "Square Root: " << "Square root of " << y << " is " << sqrt(y) << endl;
+    cout << "Square: " << x << "^" << y << "=" << pow(x, y) << endl;
 
     return 0;
 }
