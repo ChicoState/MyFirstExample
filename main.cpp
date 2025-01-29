a21a16409ce04020c385f566af7fbf5d87ed6ea8
Daniel Lopez <dlopez4@csuchico.edu>
2025-01-29 13:38:39 -0800

    fixed issue #62

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@ int main()
     int x,y;
 
     std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " <<x % y << std::endl;
+    std::cout << x << "+ "<< y <<"= "<< x + y << std::endl;
+    std::cout << x << "-" << y <<"= "<< x - y << std::endl;
+    std::cout << x << "*" << y<<"= " << x * y << std::endl;
+    std::cout << x << "/ " << y << "= "<< x / y << std::endl;
+    std::cout <<x<< "/ "<< y <<"= "<<x % y << std::endl;
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
 
