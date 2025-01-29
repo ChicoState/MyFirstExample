f5aa0d51a2b5b9813c0f0e553bc5d07221b421f7
Lawjay Lee <lawjaylee1@icloud.com>
2025-01-29 11:46:24 -0800

    fixed division by 0 error, fixes #61

--- main.cpp
+++ main.cpp
@@ -15,11 +15,10 @@ int main() {
   cout << "Subtraction: " << x - y << endl;
   cout << "Multiplication: " << x * y << endl;
   if (y == 0) {
-    cout << "Multiplication: " << x * y << endl;
     cout << "Division: " << x / y << endl;
+    cout << "Remainder: " << x % y << endl;
   }
   cout << "Square Root: " << sqrt(x) << endl;
   cout << "Square: " << pow(x, y) << endl;
-
   return 0;
 }
