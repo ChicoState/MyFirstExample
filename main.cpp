37c9550d0aa0e85240d624274b866a5ad9db3960
Marco Calderon <calderonu209@gmail.com>
2025-01-29 11:46:25 -0800

    Full math operation, fixes #62

--- main.cpp
+++ main.cpp
@@ -9,13 +9,13 @@ int main()
     int x,y;
 
     std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " <<x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
+    std::cout << x + y << std::endl;
+    std::cout << x - y << std::endl;
+    std::cout << x * y << std::endl;
+    std::cout << x / y << std::endl;
+    std::cout << x % y << std::endl;
+    std::cout << sqrt(x) << std::endl;
+    std::cout << pow(x, y) << std::endl;
 
     return 0;
 }
