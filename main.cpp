b8d11c757a7e0d36fce2b27252f9e2e94ec55119
Lawjay Lee <lawjaylee1@icloud.com>
2025-01-29 11:43:01 -0800

    fix std

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
-int main()
-{
-    cout << "This is Kevin!\n";
-    cout<<"Hi, please enter two whole numbers: ";
+int main() {
+    std::cout << "This is Kevin!\n";
+    std::cout << "Hi, please enter two whole numbers: ";
 
-    int x,y;
+    int x, y;
 
     std::cin >> x >> y;
     std::cout << "Addition: " << x + y << std::endl;
     std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " <<x * y << std::endl;
+    std::cout << "Multiplication: " << x * y << std::endl;
     std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " <<x % y << std::endl;
+    std::cout << "Remainder: " << x % y << std::endl;
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
 
