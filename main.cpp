a696575a9c04cc81a6ae68c95c6cd550cf2a4dce
copper-glitch <cooper.j.pie@gmail.com>
2025-01-29 11:47:22 -0800

    #61

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 int main()
 {
-    cout << "This is Kevin!\n";
-    cout<<"Hi, please enter two whole numbers: ";
+    std::cout << "This is Kevin!\n";
+    std::cout<<"Hi, please enter two whole numbers: ";
 
     int x,y;
 
@@ -12,7 +12,14 @@ int main()
     std::cout << "Addition: " << x + y << std::endl;
     std::cout << "Subtraction: " << x - y << std::endl;
     std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
+    if(x != 0 && y != 0)
+    {
+        std::cout << "Division: " << x / y << std::endl;
+    }
+    else
+    {
+        std::cout<<"CANT DO THAT"<<std::endl;
+    }
     std::cout << "Remainder: " <<x % y << std::endl;
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
