50a8adc877fd52e59ce4acedf1034dd73156263c
Angelina Gonzalez <amgonzalez18@csuchico.edu>
2025-02-03 11:35:16 -0800

    Fix bug in main.cpp functionality

--- main.cpp
+++ main.cpp
@@ -3,19 +3,18 @@
 
 int main()
 {
-    cout << "This is Kevin!\n";
+    cout << "This is Angelina!\n";
     cout<<"Hi, please enter two whole numbers: ";
 
     int x,y;
-
     std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " <<x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
+    std::cout << x << " + " << y << " = " << x + y << std::endl;
+    std::cout << x << " - " << y << " = " << x - y << std::endl;
+    std::cout << x << " * " << y << " = " << x * y << std::endl;
+    std::cout << x << " / " << y << " = " << x / y << " with remainder of " << x % y << std::endl;
+    std::cout << "Square root of " << x << " is " << sqrt(x) << std::endl;
+    std::cout << "Square root of " << y << " is " << sqrt(y) << std::endl;
+    std::cout << x << " ^ 2 = " << pow(x, 2) << std::endl;
 
-    return 0;
+        return 0;
 }
