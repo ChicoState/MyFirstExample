ad9239f84d4908fac71e76176771476f3ead2bd1
Patrick Guevarra <ypvguevarra@csuchico.edu>
2025-02-03 12:38:22 -0800

    Handles displaying full math operation, fixes #62

--- main.cpp
+++ main.cpp
@@ -7,34 +7,21 @@ using std::endl;
 
 int main()
 {
-<<<<<<< HEAD
-  std::cout << "This is Kevin!\n";
-  std::cout<<"Hi, please enter two whole numbers: ";
 
-    int x,y;
-
-    std::cin >> x >> y;
-    std::cout << "Addition: " << x << "+" << y << "=" << x + y << std::endl;
-    std::cout << "Subtraction: " << x << "-" << y << "=" << x - y << std::endl;
-    std::cout << "Multiplication: " << x << "*" << y << "=" << x * y << std::endl;
-    std::cout << "Division: " << x << "/" << y << "=" << x / y << "with a remainder of " << x%y << std::endl;
-    std::cout << "Square Root of " << x << " is " <<sqrt(x) << std::endl;
-    std::cout << "Square Root of " << y << " is " <<sqrt(y) << std::endl;
-    std::cout << x << "^" << y << "=" << pow(x, y) << std::endl;
-=======
-    cout << "Hi, please enter two whole numbers: ";
+  cout << "This is Kevin!\n";
+  cout<<"Hi, please enter two whole numbers: ";
 
     int x,y;
 
     cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
-    cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " << x % y << endl;
-    cout << "Square Root: " << sqrt(x) << endl;
-    cout << "Square: " << pow(x, y) << endl;
->>>>>>> b5466e8cb9209dcb082320cce7243b5d3f3eafcb
+    cout << "Addition: " << x << "+" << y << "=" << x + y << std::endl;
+    cout << "Subtraction: " << x << "-" << y << "=" << x - y << std::endl;
+    cout << "Multiplication: " << x << "*" << y << "=" << x * y << std::endl;
+    cout << "Division: " << x << "/" << y << "=" << x / y << "with a remainder of " << x%y << std::endl;
+    cout << "Square Root of " << x << " is " <<sqrt(x) << std::endl;
+    cout << "Square Root of " << y << " is " <<sqrt(y) << std::endl;
+    cout << x << "^" << y << "=" << pow(x, y) << std::endl;
+
 
     return 0;
 }
