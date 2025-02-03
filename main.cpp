aff071a5e185b9dba56ef89c60b128ada3f44c44
MoneyHalver <philipbell7891@gmail.com>
2025-02-03 11:20:21 -0800

    Handles NaN errors when dividing by 0, resolves #61

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 int main()
 {
-    cout << "This is Kevin!\n";
-    cout<<"Hi, please enter two whole numbers: ";
+    std::cout << "This is Kevin!\n";
+    std::cout<<"Hi, please enter two whole numbers: ";
 
     int x,y;
 
@@ -12,8 +12,12 @@ int main()
     std::cout << "Addition: " << x + y << std::endl;
     std::cout << "Subtraction: " << x - y << std::endl;
     std::cout << "Multiplication: " <<x * y << std::endl;
+    if (y == 0) {
+	    std::cout << "Dividing by zero is not a number.\n";
+    } else {
     std::cout << "Division: " << x / y << std::endl;
     std::cout << "Remainder: " <<x % y << std::endl;
+    }
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
 
