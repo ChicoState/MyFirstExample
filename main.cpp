6fd4e673eb263099f7c351196272eac4fab4b0c5
NickK21 <dodfannk@gmail.com>
2025-02-03 11:43:26 -0800

    Handled NaN error, fixes #61

--- main.cpp
+++ main.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include <cmath>
 
-int main()
-{
-    cout << "This is Kevin!\n";
-    cout<<"Hi, please enter two whole numbers: ";
+int main() {
+    std::cout << "This is Nick!\n";
+    std::cout << "Hi, please enter two whole numbers: ";
 
-    int x,y;
-
+    int x, y;
     std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " <<x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
+
+    if (y == 0) {
+        std::cout << "Cannot divide by 0, try again\n";
+        return 1;
+    }
+
+    std::cout << x << " + " << y << " = " << x + y << std::endl;
+    std::cout << x << " - " << y << " = " << x - y << std::endl;
+    std::cout << x << " * " << y << " = " << x * y << std::endl;
+    std::cout << x << " / " << y << " = " << x / y 
+              << " with remainder " << x % y << std::endl;
+    std::cout << "Square root of " << x << " is " << sqrt(x) << std::endl;
+    std::cout << "Square root of " << y << " is " << sqrt(y) << std::endl;
+    std::cout << x << "^" << y << " = " << pow(x, y) << std::endl;
 
     return 0;
 }
