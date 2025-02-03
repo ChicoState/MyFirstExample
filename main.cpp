112775d7b71d940607a4f4b4f4f8fa2bde15e148
Trevor Olmo <babooshkiz25@gmail.com>
2025-02-03 11:32:46 -0800

    Resolves #61

--- main.cpp
+++ main.cpp
@@ -3,19 +3,23 @@
 
 int main()
 {
-    cout << "This is Kevin!\n";
-    cout<<"Hi, please enter two whole numbers: ";
+	std::cout << "This is Kevin!\n";
+	std::cout<<"Hi, please enter two whole numbers: ";
 
-    int x,y;
+	int x,y;
 
-    std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " <<x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
+	std::cin >> x >> y;
+	std::cout << "Addition: " << x + y << std::endl;
+	std::cout << "Subtraction: " << x - y << std::endl;
+	std::cout << "Multiplication: " <<x * y << std::endl;
+	if(y == 0){
+		std::cout << "Dividing by zero is not a number." << std::endl;
+	}else{
+		std::cout << "Division: " << x / y << std::endl;
+	}
+	std::cout << "Remainder: " <<x % y << std::endl;
+	std::cout << "Square Root: " << sqrt(x) << std::endl;
+	std::cout << "Square: " << pow(x, y) << std::endl;
 
-    return 0;
+	return 0;
 }
