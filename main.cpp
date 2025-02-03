ca5c8a392817f9d5062cc4b7fafbda86b87e109c
DrewsCodeLife <drewmortenson@gmail.com>
2025-02-03 11:28:23 -0800

    Added personal comment to main, fixes nothing

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
     cout<<"Hi, please enter two whole numbers: ";
 
     int x,y;
-
+    // My custom comment
     std::cin >> x >> y;
     std::cout << "Addition: " << x + y << std::endl;
     std::cout << "Subtraction: " << x - y << std::endl;
