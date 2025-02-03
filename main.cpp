780298ab023d40109163bc53a8e19cbc7de55c34
hanzthefifth <85398332+hanzthefifth@users.noreply.github.com>
2025-02-03 11:42:03 -0800

    handles divisions by zero, fixes #61

--- main.cpp
+++ main.cpp
@@ -15,8 +15,14 @@ int main()
     cout << "Addition: " << x + y << endl;
     cout << "Subtraction: " << x - y << endl;
     cout << "Multiplication: " << x * y << endl;
+    if(y !=0){
     cout << "Division: " << x / y << endl;
     cout << "Remainder: " << x % y << endl;
+    }
+    else{
+    cout << "Division: Undefined (cannot divide by zero)" << endl;
+    cout << "Remainder: Undefined (cannot divide by zero)" << endl;
+        }
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
 
