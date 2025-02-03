b69f375d4365a8df2a1359669045743ca8930551
JorgeLuevano <jorge77gregory@gmail.com>
2025-02-03 11:44:31 -0800

    take into account the changes made to standard library

--- main.cpp
+++ main.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
 #include <cmath>
+    using std::cout;
+    using std::cin;
+    using std::cout;
+    using std::endl;
 
 int main()
 {
-    std::cout << "This is Kevin!\n";
-    std::cout<<"Hi, please enter two whole numbers: ";
+    cout << "This is Kevin!\n";
+    cout<<"Hi, please enter two whole numbers: ";
 
     int x,y;
 
-    std::cin >> x >> y;
-    std::cout << "Addition: " << x <<" + " << y << " = " << x + y << std::endl;
-    std::cout << "Subtraction: " << x <<" - " << y << " = " << x - y << std::endl;
-    std::cout << "Multiplication: " << x <<" * " << y << " = " << x * y << std::endl;
-    std::cout << "Division: " << x <<" / " << y << " = " << x / y << " with Remainder of: "<< x%y << std::endl;
+    cin >> x >> y;
+    cout << "Addition: " << x <<" + " << y << " = " << x + y << endl;
+    cout << "Subtraction: " << x <<" - " << y << " = " << x - y << endl;
+    cout << "Multiplication: " << x <<" * " << y << " = " << x * y << endl;
+    cout << "Division: " << x <<" / " << y << " = " << x / y << " with Remainder of: "<< x%y << endl;
     //std::cout << "Remainder: " <<x % y << std::endl;
-    std::cout << "Square Root of "<<x<< " is: " << sqrt(x) << std::endl;
-    std::cout << "Square Root of "<<y<< " is: " << sqrt(y) << std::endl;
-    std::cout << x << "^" << y << " = " << pow(x, y) << std::endl;
+    cout << "Square Root of "<<x<< " is: " << sqrt(x) << endl;
+    cout << "Square Root of "<<y<< " is: " << sqrt(y) << endl;
+    cout << x << "^" << y << " = " << pow(x, y) << endl;
 
     return 0;
 }
+
+
