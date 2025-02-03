b0059b5fcc3e3f2d1595c2e9ba0d4ba523f11a1d
lmmelgoza <luismelgoza101@gmail.com>
2025-02-03 11:44:22 -0800

    Displays full math operation fixes #62

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cmath>
 
+using namespace std;
+
 int main()
 {
     cout << "This is Kevin!\n";
@@ -8,14 +10,14 @@ int main()
 
     int x,y;
 
-    std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " <<x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
+    cin >> x >> y;
+    cout << "Addition: " << x << " + " << y << " = " << x + y << endl;
+    cout << "Subtraction: " <<  x << " - " << y << " = " << x - y << endl;
+    cout << "Multiplication: " << x << " * " << y << " = " << x * y << endl;
+    cout << "Division: " << x << " / " << y << " = " << x / y << endl;
+    cout << "Remainder: " << x << " % " << y << " = " << x % y << endl;
+    cout << "Square Root: " << x << " * " << x << " = " << sqrt(x) << endl;
+    cout << "Square: " << x<< "^" << y << " = " << pow(x, y) << endl;
 
     return 0;
 }
