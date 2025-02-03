c27bfc3ef90cd9590accd280b312ad38c778855d
Madison Webb <madisonewebb28@gmail.com>
2025-02-03 11:45:54 -0800

    Added using std::cin and std::cout

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <cmath>
 
+using std::cin;
+using std::cout;
+
 int main()
 {
-    cout << "This is Kevin!\n";
     cout<<"Hi, please enter two whole numbers: ";
 
     int x,y;
 
-    std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " <<x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
+    cin >> x >> y;
+    cout << "Addition: " << x + y << std::endl;
+    cout << "Subtraction: " << x - y << std::endl;
+    cout << "Multiplication: " <<x * y << std::endl;
+    cout << "Division: " << x / y << std::endl;
+    cout << "Remainder: " <<x % y << std::endl;
+    cout << "Square Root: " << sqrt(x) << std::endl;
+    cout << "Square: " << pow(x, y) << std::endl;
 
     return 0;
 }
