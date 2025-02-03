724980b5a3a7bb87def367604293721a244bad9b
babab0uille <elizabeth.e.rod11@gmail.com>
2025-02-03 12:14:36 -0800

    Fixes #62

--- main.cpp
+++ main.cpp
@@ -3,19 +3,20 @@
 
 int main()
 {
-    cout << "This is Kevin!\n";
-    cout<<"Hi, please enter two whole numbers: ";
+    std::cout << "This is Elizabeth!\n";
+    std::cout<<"Hi, please enter two whole numbers: ";
 
     int x,y;
 
     std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " <<x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
+    std::cout << x << "+" << y << "=" << x + y << std::endl;
+    std::cout << x << "-" << y << "=" << x - y << std::endl;
+    std::cout << x << "*" << y << "=" << x * y << std::endl;
+    std::cout << x << "/" << y << "=" << x / y << " with remainder of " << x % y << std::endl;
+    // std::cout << "Remainder: " << x % y << std::endl;
+    std::cout << "Square Root: " << x << " is " << sqrt(x) << std::endl;
+    std::cout << "Square Root: " << y << " is " << sqrt(y) << std::endl;
+    std::cout << x << "^" << y << "=" << pow(x, 2) << std::endl;
 
     return 0;
 }
