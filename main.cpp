ac1d91bad48139567ab8c7e9b9c9a6e862b6f39f
mateoguzv <mguzmanvanegas@mail.csuchico.edu>
2025-02-03 12:23:54 -0800

    Fixes #62: Updated main.cpp with required changes

--- main.cpp
+++ main.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 #include <cmath>
-
+using std::cin;
+using std::cout;
+using std::endl;
 int main()
 {
-    std::cout << "This is Kevin!\n";
-    std::cout<<"Hi, please enter two whole numbers: ";
-
+    std::cout << "Hi, please enter two whole numbers: ";
     int x,y;
-
-    std::cin >> x >> y;
-    std::cout << "Addition: " << x + y << std::endl;
-    std::cout << "Subtraction: " << x - y << std::endl;
-    std::cout << "Multiplication: " <<x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " <<x % y << std::endl;
-    std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
-
+    cout << x << "+" << y << "=" << x + y << endl;
+    cout << x << "-" << y << "=" << x - y << endl;
+    cout << x << "*" << y << "=" << x * y << endl;
+    cout << x << "/" << y << "=" << x / y << " with remainder of " << x % y << endl;
+    cout << "Square root of " << x << " is " << sqrt(x) << endl;
+    cout << "Square root of " << y << " is " << sqrt(y) << endl;
+    cout << x << "^" << y << "=" << pow(x, y) << endl;
     return 0;
 }
+
