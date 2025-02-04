9935f206d7420fcdb328bc5066892008f3166b84
Yash Madan <ymadan@csuchico.edu>
2025-02-04 13:23:43 -0800

    Improved output clarity by displaying full mathematical operations

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include <cmath>
 
 using std::cin;
@@ -21,4 +21,30 @@ int main()
     cout << "Square: " << pow(x, y) << endl;
 
     return 0;
+}*/
+
+#include <iostream>
+#include <cmath>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "Hi, please enter two whole numbers: ";
+
+    int x, y;
+    cin >> x >> y;
+
+    cout << x << "+" << y << "=" << x + y << endl;
+    cout << x << "-" << y << "=" << x - y << endl;
+    cout << x << "*" << y << "=" << x * y << endl;
+    cout << x << "/" << y << "=" << x / y << " with remainder of " << x % y << endl;
+    cout << "Square root of " << x << " is " << sqrt(x) << endl;
+    cout << "Square root of " << y << " is " << sqrt(y) << endl;
+    cout << x << "^" << y << "=" << pow(x, y) << endl;
+
+    return 0;
 }
+
