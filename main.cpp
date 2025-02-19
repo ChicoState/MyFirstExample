4baffc7f4c615ec341022031c3ecb19cfedfaf43
Angelina Gonzalez <amgonzalez18@csuchico.edu>
2025-02-18 19:35:20 -0800

    updating main.cpp practice fixes #61

--- main.cpp
+++ main.cpp
@@ -8,16 +8,25 @@ using std::endl;
 int main()
 {
     cout << "This is Angelina!\n";
-    cout<<"Hi, please enter two whole numbers: ";
+    cout << "Hi, please enter two whole numbers: ";
 
-    int x,y;
-    std::cin >> x >> y;
-    std::cout << x << " + " << y << " = " << x + y << std::endl;
-    std::cout << x << " - " << y << " = " << x - y << std::endl;
-    std::cout << x << " * " << y << " = " << x * y << std::endl;
-    std::cout << x << " / " << y << " = " << x / y << " with remainder of " << x % y << std::endl;
-    std::cout << "Square root of " << x << " is " << sqrt(x) << std::endl;
-    std::cout << "Square root of " << y << " is " << sqrt(y) << std::endl;
-    std::cout << x << " ^ 2 = " << pow(x, 2) << std::endl;
-        return 0;
+    int x, y;
+    cin >> x >> y;
+
+    cout << x << " + " << y << " = " << x + y << endl;
+    cout << x << " - " << y << " = " << x - y << endl;
+    cout << x << " * " << y << " = " << x * y << endl;
+
+    if (y == 0) {
+        cout << "Dividing by zero is not a number." << endl;
+    } else {
+        cout << x << " / " << y << " = " << x / y << " with remainder of " << x % y << endl;
+    }
+
+    cout << "Square root of " << x << " is " << sqrt(x) << endl;
+    cout << "Square root of " << y << " is " << sqrt(y) << endl;
+    cout << x << " ^ 2 = " << pow(x, 2) << endl;
+
+    return 0;
 }
+
