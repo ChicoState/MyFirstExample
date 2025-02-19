adab2bd28ed75097048855dc4c8f152980fa182f
cesar <cesargfranco270@gmail.com>
2025-02-18 21:40:18 -0800

    Added names Cesar & Kevin

--- main.cpp
+++ main.cpp
@@ -8,17 +8,13 @@ using std::endl;
 
 int main()
 {
-<<<<<<< HEAD
-    cout << "This is Cesar!\n";
+    cout << "This is Cesar & Kevin!\n";
     cout<<"Hi, please enter two whole numbers: ";
-=======
     cout << "Hi, please enter two whole numbers: ";
->>>>>>> b5466e8cb9209dcb082320cce7243b5d3f3eafcb
 
     int x,y;
 
     cin >> x >> y;
-<<<<<<< HEAD
     cout << "Addition: " << x << "+" << y << "=" << x + y << endl;
     cout << "Subtraction: " << x << "-" << y << "=" << x - y << endl;
     cout << "Multiplication: " << x << "*" << y << "=" << x * y << endl;
@@ -26,15 +22,13 @@ int main()
     cout << "Square Root: " << "Square root of " << x << " is " << sqrt(x) << endl;
     cout << "Square Root: " << "Square root of " << y << " is " << sqrt(y) << endl;
     cout << "Square: " << x << "^" << y << "=" << pow(x, y) << endl;
-=======
     cout << "Addition: " << x + y << endl;
     cout << "Subtraction: " << x - y << endl;
     cout << "Multiplication: " << x * y << endl;
+    cout << "Dividing by zero is not a number." << endl;
     cout << "Division: " << x / y << endl;
     cout << "Remainder: " << x % y << endl;
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
->>>>>>> b5466e8cb9209dcb082320cce7243b5d3f3eafcb
-
     return 0;
 }
