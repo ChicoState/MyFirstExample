756036457f85740fe31ebc8a2b34eb98ff0b9c45
stakacs2021 <takacsstephan@gmail.com>
2025-09-08 11:30:32 -0700

    actually completed full math display closes #62

--- main.cpp
+++ main.cpp
@@ -15,9 +15,9 @@ int main()
     cout << x << "+" << y << "=" << x + y << endl;
     cout << x << "-" << y << "=" << x - y << endl;
     cout << x << "*" << y << "=" << x * y << endl;
-    cout << x << "/" << y << "=" << x / y << "with a remainder of" << x % y << endl;
-    cout << "Square Root of " << x << "is " << sqrt(x) << endl;
-    cout << "Square Root of " << y << "is " << sqrt(y) << endl;
+    cout << x << "/" << y << "=" << x / y << " with a remainder of " << x % y << endl;
+    cout << "Square Root of " << x << " is " << sqrt(x) << endl;
+    cout << "Square Root of " << y << " is " << sqrt(y) << endl;
     cout << x << "^" << y << "=" << pow(x, y) << endl;
 
     return 0;
