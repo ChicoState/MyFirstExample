9874856f60e37c9654eb6220503cd271dd8a2078
Vincent Roberson <89275572+bincent0929@users.noreply.github.com>
2025-09-08 18:27:05 +0000

    fixed both issues 	modified:   main.cpp 	new file:   test

--- main.cpp
+++ main.cpp
@@ -12,13 +12,38 @@ int main()
     int x,y;
 
     cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
-    cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " << x % y << endl;
-    cout << "Square Root: " << sqrt(x) << endl;
-    cout << "Square: " << pow(x, y) << endl;
+    
+    //cout << "Addition: " << x + y << endl;
+    cout << x << "+" << y << "=" << x + y << endl;
+    
+    //cout << "Subtraction: " << x - y << endl;
+    cout << x << "-" << y << "=" << x - y << endl;
+    
+    //cout << "Multiplication: " << x * y << endl;
+    cout << x << "*" << y << "=" << x * y << endl;
+
+    //cout << "Division: " << x / y << endl;
+    //cout << "Remainder: " << x % y << endl;
+    
+    if (y == 0) {
+        cout << "Dividing by zero is not a number." << endl;
+    }
+    else {
+        cout << x << "/" << y << "=" << x / y;
+        if (x % y > 0) {
+        cout << " with remainder of " << x % y << endl;
+        }
+        else {
+            cout << endl;
+        }
+    }
+
+    //cout << "Square Root: " << sqrt(x) << endl;
+    cout << "Square root of " << x << " is " << sqrt(x) << endl; 
+    cout << "Square root of " << y << " is " << sqrt(y) << endl; 
+
+    //cout << "Square: " << pow(x, y) << endl;
+    cout << x << "^" << y << "=" << pow(x,y) << endl;
 
     return 0;
 }
