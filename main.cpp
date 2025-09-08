005a5b5376e249304065647c9cdece7f8aa37ca2
Ashley DelRosario <adelrosario2@csuchico.edu>
2025-09-08 11:44:44 -0700

    fixed issue, closes #62

--- main.cpp
+++ main.cpp
@@ -14,11 +14,10 @@ int main()
     int x,y;
 
     cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
+    cout << "Addition: " << x << "+" << y << "= " << x + y << endl;
+    cout << "Subtraction: " << x << "-" << y << "= " << x - y << endl;
     cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " << x % y << endl;
+    cout << "Division: " << x / y << " with remainder of " << x % y << endl;
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
 
