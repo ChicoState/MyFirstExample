217c91f0a4b8f1860dd0a2ffd768e7dd1a5314cf
Oleksandr Shestopalov <shestopalovaleks@gmail.com>
2025-09-08 11:45:17 -0700

    change the way info output, closes #62

--- main.cpp
+++ main.cpp
@@ -13,15 +13,14 @@ int main()
     cout << "Hi, please enter two whole numbers: ";
 
     int x, y;
+
     cin >> x >> y;
     cout << x << "+" << y << "=" << x + y << endl;
     cout << x << "-" << y << "=" << x - y << endl;
     cout << x << "*" << y << "=" << x * y << endl;
     cout << x << "/" << y << "=" << x / y << " with remainder of " << x % y << endl;
-    cout << std::fixed << std::setprecision(5); // not sure if it's necessary but it makes it clearer. (sets decimal places to 5).
     cout << "Square root of " << x << " is " << sqrt(x) << endl;
     cout << "Square root of " << y << " is " << sqrt(y) << endl;
-    cout.unsetf(std::ios::fixed);
     cout << x << "^" << y << "=" << pow(x, y) << endl;
 
     return 0;
