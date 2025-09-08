bdb9c469d56ae8db9ffd129d3414c41701a377f4
Michael Woldai <michael.a.woldai@gmail.com>
2025-09-08 11:49:12 -0700

    added display for math, closes #62

--- main.cpp
+++ main.cpp
@@ -12,11 +12,11 @@ int main()
     int x,y;
 
     cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
-    cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " << x % y << endl;
+    cout << "Addition: " << x << "+" << y << endl;
+    cout << "Subtraction: " << x <<"-"<< y << endl;
+    cout << "Multiplication: " << x<< "*" <<y << endl;
+    cout << "Division: " << x<< "/" << y << endl;
+    cout << "Remainder: " << x<<  "%" << y << endl;
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
 
