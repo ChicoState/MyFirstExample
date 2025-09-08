bb3b2a28a1b6ce60c7d0e3cb5f68aaa50e5449d3
atorrespena <atorrespena@csuchico.edu>
2025-09-08 11:43:45 -0700

    handles NaN errors closes #61

--- main.cpp
+++ main.cpp
@@ -10,15 +10,23 @@ int main()
     cout << "Hi, please enter two whole numbers: ";
 
     int x,y;
-
+    
     cin >> x >> y;
     cout << "Addition: " << x + y << endl;
     cout << "Subtraction: " << x - y << endl;
     cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " << x % y << endl;
-    cout << "Square Root: " << sqrt(x) << endl;
-    cout << "Square: " << pow(x, y) << endl;
+    if ( y == 0)
+    {
+        cout << " Division: Not a number error " << endl;
+    }
+    else
+     {
+        cout << "Division: " << x / y << endl;
+        cout << "Remainder: " << x % y << endl;
+        cout << "Square Root: " << sqrt(x) << endl;
+        cout << "Square: " << pow(x, y) << endl;
 
-    return 0;
+    
+     }
+    return 0;  
 }
