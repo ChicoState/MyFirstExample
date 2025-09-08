7f34108ee5e3b35867e1964705212e71c2d47e7a
Oleksandr Shestopalov <shestopalovaleks@gmail.com>
2025-09-08 11:31:28 -0700

    change the way info output, closes #62

--- main.cpp
+++ main.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>  // for std::setprecision
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::sqrt;
+using std::pow;
 
 int main()
 {
     cout << "Hi, please enter two whole numbers: ";
 
-    int x,y;
-
+    int x, y;
     cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
-    cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " << x % y << endl;
-    cout << "Square Root: " << sqrt(x) << endl;
-    cout << "Square: " << pow(x, y) << endl;
+    cout << x << "+" << y << "=" << x + y << endl;
+    cout << x << "-" << y << "=" << x - y << endl;
+    cout << x << "*" << y << "=" << x * y << endl;
+    cout << x << "/" << y << "=" << x / y << " with remainder of " << x % y << endl;
+    cout << std::fixed << std::setprecision(5); // not sure if it's necessary but it makes it clearer. (sets decimal places to 5).
+    cout << "Square root of " << x << " is " << sqrt(x) << endl;
+    cout << "Square root of " << y << " is " << sqrt(y) << endl;
+    cout.unsetf(std::ios::fixed);
+    cout << x << "^" << y << "=" << pow(x, y) << endl;
 
     return 0;
 }
+
