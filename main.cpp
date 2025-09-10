ec6c960e06842dd3d8545221ea668efec4698136
Chance Stahura <chance.stahura68@gmail.com>
2025-09-10 11:39:29 -0700

    handle divion when y ==0

--- main.cpp
+++ main.cpp
@@ -20,11 +20,11 @@ int main()
     cout << "Subtraction: " << x - y << endl;
     cout << "Multiplication: " << x * y << endl;
 
-    if(y == 0) {
-        cout << "Dividing by zero is not a number." << endl;
-    } else {
+    //if(y == 0) {
+      //  cout << "Dividing by zero is not a number." << endl;
+    //} else {
         cout << "Division: " << x / y << endl;
-    }
+    //}
     cout << "Remainder: " << x % y << endl;
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
