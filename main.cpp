757c07b5e68ddb6f98a4f607e54849a5651a2057
Cole Doud <cjdoud@csuchico.edu>
2025-09-10 11:46:33 -0700

    fixes divide by zero issue, closes #61

--- main.cpp
+++ main.cpp
@@ -10,15 +10,23 @@ int main()
     cout << "Hi, please enter two whole numbers: ";
 
     int x,y;
-
+	
     cin >> x >> y;
-    cout << x << "+" << y << "=" << x + y << endl;
-    cout << x << "-" << y << "=" << x - y << endl;
-    cout << x << "*" << y << "=" << x * y << endl;
-    cout << x << "/" << y << "=" << x / y << " with remainder of " << x % y << endl;
-    cout << "Square root of " << x << " is " << sqrt(x) << endl;
-    cout << "Square root of " << y << " is " << sqrt(y) << endl;
-    cout << x << "^" << y << "=" << pow(x,y) << endl;
+
+	if (x * y != 0)
+	{
+    	cout << x << "+" << y << "=" << x + y << endl;
+    	cout << x << "-" << y << "=" << x - y << endl;
+    	cout << x << "*" << y << "=" << x * y << endl;
+    	cout << x << "/" << y << "=" << x / y << " with remainder of " << x % y << endl;
+    	cout << "Square root of " << x << " is " << sqrt(x) << endl;
+    	cout << "Square root of " << y << " is " << sqrt(y) << endl;
+    	cout << x << "^" << y << "=" << pow(x,y) << endl;
+
+	} else
+	{
+	cout << "Please enter non-zero numbers!" << endl;
+	}
 
     return 0;
 }
