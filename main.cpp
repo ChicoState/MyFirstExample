984cbdedeac779d6ffdbdbda7a8745a44a4c43f7
Joseph Bautista <jdbautista1@csuchico.edu>
2025-09-10 14:33:02 -0700

    Fix Issue: Display, Closes #62

--- main.cpp
+++ main.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
 #include <cmath>
 
-using std::cin;
-using std::cout;
-using std::endl;
 
-int main()
-{
-    cout << "Hi, please enter two whole numbers: ";
+int main() {
 
     int x,y;
 
-    cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
-    cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " << x % y << endl;
-    cout << "Square Root: " << sqrt(x) << endl;
-    cout << "Square: " << pow(x, y) << endl;
+    std::cin >> x >> y;
+    std::cout << x << " + " << y << "=" << x + y << std::endl;
+    std::cout << x << "-" << y << "=" << x - y << std::endl;
+    std::cout << x << " * " << y << "=" << x * y << std::endl;
+    std::cout << x << " / " << y << "=" << x / y << " with remainder of " << x % y << std::endl;
+    std::cout << "Square Root of " << x << " is " << sqrt(x) << std::endl;
+    std::cout << "Square Root of " << y << " is " << sqrt(y) << std::endl;
+    std::cout << x << "^" << y << " = " << pow(x, y) << std::endl;
 
     return 0;
 }
