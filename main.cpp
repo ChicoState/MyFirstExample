664a6893cefc4f6797cad7220867e1804e5b9412
emmaisaway <mtillman@csuchico.edu>
2025-09-10 11:11:28 -0700

    Fix divide by zero error

--- main.cpp
+++ main.cpp
@@ -15,7 +15,11 @@ int main()
     cout << "Addition: " << x + y << endl;
     cout << "Subtraction: " << x - y << endl;
     cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
+    if (y == 0) {
+        cout << "Division: " << x / y << endl;
+    } else {
+	cout << "Division: divide by Zero error" << endl;
+    }
     cout << "Remainder: " << x % y << endl;
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
