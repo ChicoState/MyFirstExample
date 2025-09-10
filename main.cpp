0a64b64d8acdce7fa06060361b3856c0adb194b1
NcollegeB <skyryze02@gmail.com>
2025-09-10 11:23:12 -0700

    fixed Nan

--- main.cpp
+++ main.cpp
@@ -6,14 +6,24 @@ int main()
     std::cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
     std::cout << "Hi, please enter two whole numbers: ";
 
-    int x,y;
+    int x, y;
 
     std::cin >> x >> y;
     std::cout << "Addition: " << x + y << std::endl;
     std::cout << "Subtraction: " << x - y << std::endl;
     std::cout << "Multiplication: " << x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
-    std::cout << "Remainder: " << x % y << std::endl;
+
+    if (y == 0)
+    {
+        std::cout << "Division: Undefined (division by zero)" << std::endl;
+        std::cout << "Remainder: Undefined (division by zero)" << std::endl;
+    }
+    else
+    {
+        std::cout << "Division: " << x / y << std::endl;
+        std::cout << "Remainder: " << x % y << std::endl;
+    }
+
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
 
