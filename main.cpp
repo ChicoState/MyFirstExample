fdcea4ee07fc1186e8331b110ff2efe02a84c91c
Joseph Bautista <jdbautista1@csuchico.edu>
2025-09-23 20:53:51 -0700

    Fixed Issues: Display Math, closes #62

--- main.cpp
+++ main.cpp
@@ -2,18 +2,21 @@
 #include <cmath>
 
 
-int main() {
+int main()
+{
+    std::cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
+    std::cout << "Hi, please enter two whole numbers: ";
 
     int x,y;
 
     std::cin >> x >> y;
-    std::cout << x << " + " << y << "=" << x + y << std::endl;
-    std::cout << x << "-" << y << "=" << x - y << std::endl;
-    std::cout << x << " * " << y << "=" << x * y << std::endl;
-    std::cout << x << " / " << y << "=" << x / y << " with remainder of " << x % y << std::endl;
-    std::cout << "Square Root of " << x << " is " << sqrt(x) << std::endl;
-    std::cout << "Square Root of " << y << " is " << sqrt(y) << std::endl;
-    std::cout << x << "^" << y << " = " << pow(x, y) << std::endl;
+    std::cout << x << " + " << y << " = " << x + y << std::endl;
+    std::cout << x << " - " << y << " = " << x - y << std::endl;
+    std::cout << x << " * " << y << " = " << x * y << std::endl;
+    std::cout << x << " / " << y << " = " << x / y << " with reminder of " << x % y << std::endl;
+    std::cout << "Square root of " << x << " is " << sqrt(x) << std::endl;
+    std::cout << "Square root of " << y << " is " << sqrt(y) << std::endl;
+    std::cout <<  x << "^" << y << "=" << pow(x, y) << std::endl;
 
     return 0;
 }
