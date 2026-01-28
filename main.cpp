0e8a2172660423d72527db458327a89904bca0be
CjBoyle3 <cjboyle@csuchico.edu>
2026-01-28 11:42:47 -0800

    Prevents divide by zero, resolves #61

--- main.cpp
+++ main.cpp
@@ -6,13 +6,19 @@ int main()
     std::cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
     std::cout << "Hi, please enter two whole numbers: ";
 
-    int x,y;
+    int x, y;
 
     std::cin >> x >> y;
     std::cout << "Addition: " << x + y << std::endl;
     std::cout << "Subtraction: " << x - y << std::endl;
     std::cout << "Multiplication: " << x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
+    if (y == 0) {
+        std::cout << "Dividing by zero is not a number.\n";
+    }
+    else {
+        std::cout << "Division: " << x / y << std::endl;
+    }
+
     std::cout << "Remainder: " << x % y << std::endl;
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
