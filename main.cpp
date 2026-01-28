f127b3ffee09264702d03c177141cdb70375eee1
robert00000 <boewilliam@gmail.com>
2026-01-28 11:41:01 -0800

    Fixed NaN error by making it so that the program doesn't divide by 0.

--- main.cpp
+++ main.cpp
@@ -12,7 +12,11 @@ int main()
     std::cout << "Addition: " << x + y << std::endl;
     std::cout << "Subtraction: " << x - y << std::endl;
     std::cout << "Multiplication: " << x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
+    if(y == 0)
+      std::cout << "You cannot divide by zero input a different number" << std::endl;
+    else{
+      std::cout << "Division: " << x / y << std::endl;
+    }
     std::cout << "Remainder: " << x % y << std::endl;
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
