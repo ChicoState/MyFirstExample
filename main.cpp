53cb5e9293324af36b7727afd3ed31e6148bf582
jasjit <jdhillon5@csuchico.edu>
2026-01-28 11:45:36 -0800

    gave explicit size , resolves #340

--- main.cpp
+++ main.cpp
@@ -4,9 +4,10 @@
 int main()
 {
     std::cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
-    std::cout << "Hi, please enter two whole numbers: ";
+    std::cout << "Hi, please enter two whole numbers: \n";
+	std::cout << "(Limited to whole numbers between -2,147,483,648 and 2,147,483,647)\n";
 
-    int x,y;
+    int32_t x,y;
 
     std::cin >> x >> y;
     std::cout << "Addition: " << x + y << std::endl;
