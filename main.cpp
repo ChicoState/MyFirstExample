977cb45076befd64c9496924bc8574d2a4b0471f
Kenai Toledo <ktoledo@csuchico.edu>
2026-02-02 12:35:06 -0800

    closes #61

--- main.cpp
+++ main.cpp
@@ -17,7 +17,15 @@ int main()
     cout << "Addition: " << x + y << endl;
     cout << "Subtraction: " << x - y << endl;
     cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
+
+    if (y != 0) {
+      cout << "Division: " << static_cast<double>(x) / y << endl;
+      cout << "Remainder: " << x % y << endl;
+    } else {
+      cout << "Division: Dividing by zero is not a number." << endl;
+      cout << "Remainder: Dividing by zero is not a number." << endl;
+    }
+    
     cout << "Remainder: " << x % y << endl;
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
