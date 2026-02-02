ed3af12f97cf0dc47fe982a16af641260c1405e2
RezDead <razorjcgames@gmail.com>
2026-02-02 11:17:35 -0800

    Test Commit

--- main.cpp
+++ main.cpp
@@ -17,5 +17,7 @@ int main()
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
 
+    std::cout << "Test information" << std:: endl;
+
     return 0;
 }
