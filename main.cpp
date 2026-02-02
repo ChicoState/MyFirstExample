35d9e357a669ee88b44e644cdb99efac789d324f
DatHypnoboi <25212257+DatHypnoboi@users.noreply.github.com>
2026-02-02 11:41:01 -0800

    thing

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <cmath>
+#include <cstdint>
 
 int main()
 {
     std::cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
     std::cout << "Hi, please enter two whole numbers: ";
 
-    int x,y;
+    uint32_t x,y;
 
     std::cin >> x >> y;
     std::cout << "Addition: " << x + y << std::endl;
