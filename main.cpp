a820aee4dc16e78be522804b1cdd60f2e1c39168
Elijah Beverley <ebeverley@csuchico.edu>
2026-02-02 12:46:38 -0800

    Fix NaN error by handling division by zero
    Closes #61

--- main.cpp
+++ main.cpp
@@ -16,6 +16,14 @@ int main()
     std::cout << "Remainder: " << x % y << std::endl;
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
+    if ( y== 0) {
+
+        std::cout << "Dividing by zero is not a number.\n";
+
+    } else{
+
+        std::cout << "Division: " << x/y << std::endl;
+    }
 
     return 0;
 }
