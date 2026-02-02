42d3952a6d24611d6f2c5018791718dd90e09e37
Skye Santilla <sesantilla@csuchico.edu>
2026-02-02 15:50:56 -0800

    Clean up divide-by-zero handling logic

--- main.cpp
+++ main.cpp
@@ -17,15 +17,12 @@ int main()
     cout << "Subtraction: " << x - y << endl;
     cout << "Multiplication: " << x * y << endl;
 
-    if( y == 0 ){
-
+    if (y == 0) {
+        cout << "Dividing by zero is not a number." << endl;
+    } else {
     cout << "Division: " << x / y << endl;
-        if( y == 0 ){
-      cout << "Dividing by zero is not a number.\n";
     }
-    else{
-      cout << "Division: " << x / y << std::endl;
-    }
+
     cout << "Remainder: " << x % y << endl;
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
