48f21bdb13aff49173a2cd8888453a61c22214f3
Susana Sandoval <sfsandoval@csuchico.edu>
2026-02-02 11:14:53 -0800

    first push

--- main.cpp
+++ main.cpp
@@ -12,10 +12,31 @@ int main()
     std::cout << "Addition: " << x + y << std::endl;
     std::cout << "Subtraction: " << x - y << std::endl;
     std::cout << "Multiplication: " << x * y << std::endl;
+    if(y == 0) { 
+        std::cout << "Dividing by zero is ot a number.\n"; 
+    } else { 
     std::cout << "Division: " << x / y << std::endl;
+    } 
     std::cout << "Remainder: " << x % y << std::endl;
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
 
     return 0;
 }
+
+/* 
+
+Compiling and running 
+g++ main.cpp 
+./a.out 
+
+To add the file to the staging area before pushing 
+git add main.cpp 
+
+Commit changes with message
+git commit -m "description of what the changes accomplished"
+
+Push changes to github 
+git push main.cpp 
+
+/* 
