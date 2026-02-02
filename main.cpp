f60fc65e6cf94d941f7229f9049aab5b0794a807
PersnicketyChkn <chrisajohnson234@gmail.com>
2026-02-02 11:15:47 -0800

    Prevents divide by 0 errors, resolves #61

--- main.cpp
+++ main.cpp
@@ -12,7 +12,9 @@ int main()
     std::cout << "Addition: " << x + y << std::endl;
     std::cout << "Subtraction: " << x - y << std::endl;
     std::cout << "Multiplication: " << x * y << std::endl;
-    std::cout << "Division: " << x / y << std::endl;
+    if(y==)
+	    std::cout << "Error: Divide by 0\n";
+    else{   std::cout << "Division: " << x / y << std::endl;}
     std::cout << "Remainder: " << x % y << std::endl;
     std::cout << "Square Root: " << sqrt(x) << std::endl;
     std::cout << "Square: " << pow(x, y) << std::endl;
