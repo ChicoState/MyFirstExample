d16560dd112b520eef5eb417307d446613550e2d
Maxwell <maxwell.fitzpatrick117@gmail.com>
2026-02-02 13:47:02 -0800

    added using std::cin; using std::cout; using std::endl; added double space indents (rather than tabs)

--- main.cpp
+++ main.cpp
@@ -6,23 +6,24 @@ using std::endl;
 
 int main()
 {
-    cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
-    cout << "Hi, please enter two whole numbers: ";
+  cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
+  cout << "Hi, please enter two whole numbers: ";
 
-    int x,y;
+  int x,y;
 
-    cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
-    cout << "Multiplication: " << x * y << endl;
-    if ( y== 0 ) {
-        cout << "Divding by zero is niot a number\n";
-    }
-    else {cout << "Division: " << x / y << endl;
-    }
-    cout << "Remainder: " << x % y << endl;
-    cout << "Square Root: " << sqrt(x) << endl;
-    cout << "Square: " << pow(x, y) << endl;
+  cin >> x >> y;
+  cout << "Addition: " << x + y << endl;
+  cout << "Subtraction: " << x - y << endl;
+  cout << "Multiplication: " << x * y << endl;
+  if ( y== 0 ) {
+    cout << "Divding by zero is niot a number\n";
+  }
+  else { 
+    cout << "Division: " << x / y << endl;
+  }
+  cout << "Remainder: " << x % y << endl;
+  cout << "Square Root: " << sqrt(x) << endl;
+  cout << "Square: " << pow(x, y) << endl;
 
-    return 0;
+  return 0;
 }
