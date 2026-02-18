116b6ce093ff383b5c0c0de11d13f439bbc60d28
tojusts <pvt.orozco.thomas@gmail.com>
2026-02-17 16:25:09 -0800

    Fixes divide by zero error, Resolves #61

--- main.cpp
+++ main.cpp
@@ -16,16 +16,14 @@ int main()
     cout << "Addition: " << x + y << endl;
     cout << "Subtraction: " << x - y << endl;
     cout << "Multiplication: " << x * y << endl;
-    if( y == 0 ){
-      cout << "Dividing by zero is not a number.\n";
+    if (y == 0) {
+        cout << "Cannot divide by zero\n";
+    } else {
+        cout << "Division: " << x / y << endl;
+        cout << "Remainder: " << x % y << endl;
     }
-    else{
-      cout << "Division: " << x / y << std::endl;
-    }
-    cout << "Remainder: " << x % y << endl;
     cout << "Square Root: " << sqrt(x) << endl;
     cout << "Square: " << pow(x, y) << endl;
-
 
     return 0;
 }
